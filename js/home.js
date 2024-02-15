ea0756c3c0d8ca6b85d70f7094f915a30575fab9
thetim1911 <thetim191@gmail.comm>
2024-02-15 22:06:18 +0700

    first commit

---
+++ js/home.js
@@ -0,0 +1,13 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+
+    let myMenu = document.getElementById('myMenu')
+    if(myMenu.className === 'menu'){
+        myMenu.className += ' menu-active'
+    } else {
+        myMenu.className = 'menu'
+    } 
+
+
+  }
+  
---
+++ js/medicine/amlo5.js
@@ -0,0 +1,11 @@
+function toggle(x) {
+    x.classList.toggle("change");
+   
+    let myMenu = document.getElementById('myMenu')
+         if(myMenu.className === 'menu'){
+                      myMenu.className += ' menu-active'
+         } else {
+              myMenu.className = 'menu'
+               } 
+     }
+   
